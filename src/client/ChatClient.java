1b20eb07eb53a874d26a9dc84d73849209b21d4c
Deny <sokundeny18@gmail.com>
2025-02-03 14:03:56 +0700

    update chat

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -2,8 +2,8 @@ package client;
 
 public class ChatClient {
 	
-	private static String SERVER_ADDRESS = "172.23.1.74";
-	private static int SERVER_PORT = 3001;
+	static String SERVER_ADDRESS = "172.23.1.74";
+	static int SERVER_PORT = 3001;
 	
 	public static void main(String[] args){
 //		System.out.println("Client");
